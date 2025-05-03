330683cad33338cf687d5bf610fa7ab017bd7784
jimin <hawoosay2@gmail.com>
2025-05-03 15:20:58 +0900

    exapp 삭제

--- src/main/java/com/exapp/comsumption/note_add/NoteAddRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exapp.comsumption.note_add;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteAddRepository extends JpaRepository<NoteAdd, Long>{
-
-}
